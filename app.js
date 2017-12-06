e42c1c35f5675fc067fb2897d000917f66c8034a
YieZhiqiu <30974111+YieZhiqiu@users.noreply.github.com>
2017-12-06 17:06:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,24 +4,18 @@ var url = require('url')
 var path = require('path')
 
 http.createServer(function(req, res){
-
   var pathObj = url.parse(req.url, true)
-  console.log(pathObj)
-
+  console.log(pathObj);
   switch (pathObj.pathname) {
     case '/loadMore':
-
       var curIdx = pathObj.query.idx
       var len = pathObj.query.len
       var data = []
-
       for(var i = 0; i < len; i++) {
         data.push('新闻' + (parseInt(curIdx) + i))
       }
-      res.end(JSON.stringify(data))
-          
+      res.end(JSON.stringify(data))   
       break;
-
     default:
      fs.readFile(path.join(__dirname ,'static', pathObj.pathname), function(err, data){
       if(err){
